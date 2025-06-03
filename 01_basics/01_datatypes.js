3d5ae22f13a043f4a05e110606b7198458ef3bee
keshav6204 <keshavtomar101@gmail.com>
2025-06-03 12:52:53 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,8 @@
+ "use strict"; // treat all js code as newer version
+
+//  alert("hello") // we are using nodejs, not browser
+let age=18;
+console.log(typeof undefinded); // undefinded
+console.log(typeof null); //object
+
+console.log(typeof age);
